12293a515178b48887af4e21b77ed4523fedeb9c
Decho Kocharin <sourcedecho@outlook.com>
2020-04-10 00:05:58 +0700

    Update arctichash.c
    *not found shares

--- algo/arctichash.c
+++ algo/arctichash.c
@@ -11,8 +11,8 @@
 #include "sha3/sph_cubehash.h"
 #include "sha3/sph_simd.h"
 #include "sha3/sph_whirlpool.h"
-#include "sha3/sph_gost.h"
-#include "lyra2/lyra2.h"
+#include "sha3/gost_streebog.h"
+#include "lyra2/Lyra2.h"
 #include "sha3/sph_groestl.h"
 #include "sha3/sph_echo.h"
 
@@ -47,7 +47,7 @@ void arctichash_hash(void *output, const void *input)
     sph_echo512(&ctx_echo, hash + 1*nOutLen, nOutLen);
     sph_echo512_close(&ctx_echo, hash + 2);
     
-    LYRA2RE(hash + 3*nOutLen, nOutLen, hash + 2*nOutLen, nOutLen, hash + 2*nOutLen, nOutLen, 1, 8, 8);
+    LYRA2(hash + 3*nOutLen, nOutLen, hash + 2*nOutLen, nOutLen, hash + 2*nOutLen, nOutLen, 1, 8, 8);
     
     sph_groestl512_init(&ctx_groestl);
     sph_groestl512(&ctx_groestl, hash + 3*nOutLen, nOutLen);
@@ -71,7 +71,7 @@ void arctichash_hash(void *output, const void *input)
     
     // Round 2
     
-    LYRA2RE(hash + 9*nOutLen, nOutLen, hash + 8*nOutLen, nOutLen, hash + 8*nOutLen, nOutLen, 1, 8, 8);
+    LYRA2(hash + 9*nOutLen, nOutLen, hash + 8*nOutLen, nOutLen, hash + 8*nOutLen, nOutLen, 1, 8, 8);
     
     sph_whirlpool_init(&ctx_whirlpool);
     sph_whirlpool(&ctx_whirlpool, hash + 9*nOutLen, nOutLen);
@@ -139,41 +139,43 @@ void arctichash_hash(void *output, const void *input)
     sph_blake512(&ctx_blake, hash + 24*nOutLen, nOutLen);
     sph_blake512_close(&ctx_blake, hash + 25*nOutLen);
     
-    LYRA2RE(output, 32, hash + 25*nOutLen, nOutLen, hash + 25*nOutLen, nOutLen, 1, 8, 8);
+    LYRA2(output, 32, hash + 25*nOutLen, nOutLen, hash + 25*nOutLen, nOutLen, 1, 8, 8);
 	
 }
 
 int scanhash_arctichash(int thr_id, struct work *work, uint32_t max_nonce, uint64_t *hashes_done)
 {
-	uint32_t _ALIGN(128) hash32[8];
+	uint32_t _ALIGN(128) hash[8];
 	uint32_t _ALIGN(128) endiandata[20];
 	uint32_t *pdata = work->data;
 	uint32_t *ptarget = work->target;
 
 	const uint32_t Htarg = ptarget[7];
 	const uint32_t first_nonce = pdata[19];
+	uint32_t nonce = first_nonce;
 
-	uint32_t n = first_nonce;
+	if (opt_benchmark)
+		ptarget[7] = 0x0000ff;
 
 	for (int i=0; i < 19; i++) {
 		be32enc(&endiandata[i], pdata[i]);
 	}
 
 	do {
-		be32enc(&endiandata[19], n);
-		arctichash_hash(hash32, endiandata);
-		if (hash32[7] < Htarg && fulltest(hash32, ptarget)) {
-			work_set_target_ratio(work, hash32);
-			*hashes_done = n - first_nonce + 1;
-			pdata[19] = n;
-			return true;
+		be32enc(&endiandata[19], nonce);
+		lyra2rev2_hash(hash, endiandata);
+
+		if (hash[7] <= Htarg && fulltest(hash, ptarget)) {
+			work_set_target_ratio(work, hash);
+			pdata[19] = nonce;
+			*hashes_done = pdata[19] - first_nonce;
+			return 1;
 		}
-		n++;
+		nonce++;
 
-	} while (n < max_nonce && !work_restart[thr_id].restart);
+	} while (nonce < max_nonce && !work_restart[thr_id].restart);
 
-	*hashes_done = n - first_nonce + 1;
-	pdata[19] = n;
-
+	pdata[19] = nonce;
+	*hashes_done = pdata[19] - first_nonce + 1;
 	return 0;
 }
